4984d458d05df11bef60802554d948153c693714
depool09 <durandepool@gmail.com>
2026-09-23 18:52:49 -0600

    Add files via upload

---
+++ EVA2_7_ELSE_IF/src/eva2_7_else_if/EVA2_7_ELSE_IF.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_else_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA2_7_ELSE_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   //TEMPERATURA
+   //35 MUY CALIDO
+   //20 AGRADABLE
+   //1O FRESCO
+   //MUY FRIOP
+    int temp;
+   Scanner captu = new Scanner(System.in);
+    System.out.println("Cual es la temperatura?");
+    temp = captu.nextInt();
+    if(temp >= 35)
+    System.out.println("Muy calido!!");
+    else if(temp >= 20)
+    System.out.println("Agradable!!");
+    else if(temp >= 10)
+    System.out.println("Frescoo!!");
+    else
+    System.out.println("muy frio!!");
+    
+    
+    }
+    
+}
